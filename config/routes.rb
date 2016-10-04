6f4041f0b61c6203a07232f47a9317ecdccc8f29
Megan Sawkar <mwelch122@gmail.com>
2016-10-04 13:29:43 -0700

    Checkpoint_Four Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  #root 'users#show'
-
   devise_for :users
   resources :users, :only => [:show]
 
-  root 'users#show'
+  root 'users#show' 
 
 
   # The priority is based upon order of creation: first created -> highest priority.
