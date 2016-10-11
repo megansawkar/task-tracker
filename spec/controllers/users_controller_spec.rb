a913d18f58bc771af3b8ac231cb6c08cdfd72306
Megan Sawkar <mwelch122@gmail.com>
2016-10-11 14:19:36 -0700

    Checkpoint Six Complete + Rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe UsersController, type: :controller do
   let(:new_user_attributes) do
     {
-      email: "blochead@bloc.io",
+      email: Faker::Internet.email,
       password: "blochead",
       password_confirmation: "blochead"
     }
   end
 
   describe "GET show" do
+
     it "returns http success" do
       get :show
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to user_session_path
     end
   end
 end
