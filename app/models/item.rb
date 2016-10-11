da827266a59ab5d0842a1f7fc3ddea3df6da7c7b
Megan Sawkar <mwelch122@gmail.com>
2016-10-11 12:48:57 -0700

    Add item_spec.rb successful test.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :user
 
   validates :name, length: { minimum: 3 }, presence: true
-  validates :user, presence: true 
+  validates :user, presence: true
+
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005180818) do
+ActiveRecord::Schema.define(version: 20161011193743) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "public"
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
